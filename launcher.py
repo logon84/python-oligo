1bb43b17b48723da61deed67f9c718a6e15d75f1
logon84 <logon84@users.noreply.github.com>
2020-07-17 21:07:52 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -19,6 +19,7 @@ connection.login(user_i_de, passw_i_de)
 #x = connection.get_hourly_consumption(datetime.strptime("15-06-2020","%d-%m-%Y"),datetime.strptime("02-07-2020","%d-%m-%Y"))
 #x = connection.get_invoice_consumption(1)
 #x = connection.get_ree_20dha_data(ree_token,datetime.strptime("15-06-2020","%d-%m-%Y"),datetime.strptime("02-07-2020","%d-%m-%Y"))
-x = connection.calculate_invoice_20dha_PVPC(ree_token,0)
-
-print(x)
+for i in range(7):
+    x = connection.calculate_invoice_PVPC(ree_token,i)
+    print(x)
+    print("\n###################################\n")
