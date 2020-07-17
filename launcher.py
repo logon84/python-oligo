556e2b8b07ef77f33124c96bdef4517c052f6428
logon84 <logon84@users.noreply.github.com>
2020-07-17 09:46:35 +0200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -15,10 +15,10 @@ connection = Iber()
 connection.login(user_i_de, passw_i_de)
 
 #x = connection.watthourmeter()
-x = connection.get_invoice(5)
+#x = connection.get_invoice(5)
 #x = connection.get_hourly_consumption(datetime.strptime("15-06-2020","%d-%m-%Y"),datetime.strptime("02-07-2020","%d-%m-%Y"))
 #x = connection.get_invoice_consumption(1)
 #x = connection.get_ree_20dha_data(ree_token,datetime.strptime("15-06-2020","%d-%m-%Y"),datetime.strptime("02-07-2020","%d-%m-%Y"))
-#x = connection.calculate_invoice_20dha_PVPC(ree_token,0)
+x = connection.calculate_invoice_20dha_PVPC(ree_token,0)
 
 print(x)
