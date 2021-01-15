b34f0960bd47cb8e6f6b999223038ff96f8646d8
logon84 <logon84@users.noreply.github.com>
2021-01-15 19:21:24 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -3,21 +3,6 @@
 from datetime import datetime
 from oligo import Iber
 
-try:
-    # Win32
-    from msvcrt import getch
-except ImportError:
-    # UNIX
-    def getch():
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            return sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old)
-
 ###############PREQUISITES###########################
 user_i_de = "i-de_user_here"
 passw_i_de = "i-de_passwd_here"
