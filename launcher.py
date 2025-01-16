e5d440b55753a2fdbfd2c5cbd76cfdaff21d912c
logon84 <logon84@users.noreply.github.com>
2025-01-16 17:04:36 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -6,7 +6,6 @@ from oligo import Iber
 ###############PREQUISITES###########################
 user_i_de = "i-de_user_here"
 passw_i_de = "i-de_passwd_here"
-ree_token = "ree_token_here"
 
 #####################################################
 
@@ -14,13 +13,9 @@ ree_token = "ree_token_here"
 connection = Iber()
 connection.login(user_i_de, passw_i_de)
 connection.get_PS_info()
-connection.get_power_peaks("2020")
-connection.continuous_calc(ree_token)
+connection.get_power_peaks()
+connection.comparator()
 
-#x = connection.calculate_invoice_PVPC(ree_token,1,0)
-#x = connection.watthourmeter()
+#x = connection.measurement()['consumption']
 #x = connection.get_invoice(5)
-#x = connection.get_hourly_consumption(datetime.strptime("15-06-2020","%d-%m-%Y"),datetime.strptime("02-07-2020","%d-%m-%Y"))
-#x = connection.get_invoice_consumption(1)
-#x = connection.get_ree_data(ree_token,datetime.strptime("15-06-2020","%d-%m-%Y"),datetime.strptime("02-07-2020","%d-%m-%Y"))
-#print(x)
+#x = connection.get_hourly_consumption(datetime.strptime("15-06-2024","%d-%m-%Y"),datetime.strptime("02-07-2024","%d-%m-%Y"))
